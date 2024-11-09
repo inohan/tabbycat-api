d67d3440aef5e2f472515e48df8282732f8ce5c4
inohaan <sato4inoue2003@gmail.com>
2024-11-09 13:31:22 +0900

    add import

--- tabbycat_api/utils/__init__.py
+++ tabbycat_api/utils/__init__.py
@@ -1,9 +1,11 @@
 from .route import route_class, cachable, create_url
 from .to_json import to_json
+from . import verify_feedbacks
 
 __all__ = [
     "route_class",
     "cachable",
     "create_url",
-    "to_json"
+    "to_json",
+    "verify_feedbacks"
 ]
